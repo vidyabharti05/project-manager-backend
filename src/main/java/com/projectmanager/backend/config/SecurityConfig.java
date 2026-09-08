55c3a66b7eaa0447a277cd24fecd6a5450c28648
vidya bharti <vidyabharti2605@gmail.com>
2026-09-08 20:02:11 +0530

    Add allowed origins for CORS configuration

--- src/main/java/com/projectmanager/backend/config/SecurityConfig.java
+++ src/main/java/com/projectmanager/backend/config/SecurityConfig.java
@@ -76,7 +76,9 @@ public SecurityFilterChain securityFilterChain(
         configuration.setAllowedOrigins(
                 List.of(
                     "http://localhost:3000",
-                    "http://127.0.0.1:3000"
+                    "http://127.0.0.1:3000", 
+                    "https://project-manager-e2k9.vercel.app",
+                    "https://project-manager-e2k9-dd4jntiot-acme-703e.vercel.app"
                 )
         );
 
